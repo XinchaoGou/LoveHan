3a40bcdf686001802db3f6dd81baacaf9bcef6ed
liuynkit <liuynkit@gmail.com>
2018-04-24 13:09:03 +0200

    第一张修海

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,9 +5,8 @@ const app = getApp()
 Page({
   data: {
     dawn: false,
-    daytime:true,
-    dusk: false,
-    night: false,
+    daytime:false,
+    dusk: true,
     bgcolor: "#b2b2b2"
   },
   //事件处理函数
@@ -20,24 +19,19 @@ Page({
    
     if (this.data.dawn)
       this.setData({
-        bgcolor: "-webkit-linear-gradient(top,rgba(133,149,165,1),rgba(188,204,220,1),rgba(234,206,202,1),rgba(246,182,180,1))"
+        bgcolor: "-webkit-gradient(linear,left top,right bottom,from(rgba(133,149,165,1)),color-stop(50%,rgba(188,204,220,1)),color-stop(70%,rgba(234,206,202,1)),to(rgba(246,182,180,1)))"
       })
     else if (this.data.daytime)
       this.setData({
-        bgcolor: "-webkit-gradient(linear,left top,right bottom,from(rgba(255,255,255,1)),color-stop(50%,rgba(254,229,102,1)),color-stop(70%,rgba(243, 200, 95,1)),to(rgba(88, 106, 6,1)))"
+        bgcolor: "-webkit-gradient(linear,left top,right bottom,from(rgba(255,255,255,1)),color-stop(30%,rgba(141,180,198,0.8)),color-stop(40%,rgba(254,229,102,0.2)),to(rgba(140,156,9,1)))"
       })
     else if (this.data.dusk)
       this.setData({
-        bgcolor: time
+        bgcolor: "-webkit-gradient(linear,left top,right bottom,from(rgba(31,104,191,1)),color-stop(20%,rgba(31,104,191,0.8)),color-stop(40%,rgba(254,108,107,1)),color-stop(60%,rgba(254,238,190,1)),color-stop(75%,rgba(49,21,24,0.5)),to(rgba(49,21,24,1)))"
       })
-    else if (this.data.night)
+    else 
       this.setData({
-        bgcolor: time
+        bgcolor: "-webkit-gradient(linear,left top,right bottom,from(rgba(15,48,94,1)),color-stop(50%,rgba(20,46,96,1)),color-stop(70%,rgba(8,26,66,1)),to(rgba(0,14,40,1)))"
       })
-    else {
-      this.setData({
-        bgcolor: time
-      })
-    }
   }
 })
